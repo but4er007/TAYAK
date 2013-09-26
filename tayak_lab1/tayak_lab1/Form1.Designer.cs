3672ae36934b141b5cb2af47034d5a123756db03
Ovechkin Pavel <but4er007@gmail.com>
2013-09-26 19:33:22 +0400

    Add tayak lab#1

---
+++ tayak_lab1/tayak_lab1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace tayak_lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.labelErrors = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button.Location = new System.Drawing.Point(172, 41);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(322, 26);
+            this.button.TabIndex = 0;
+            this.button.Text = "Result";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label
+            // 
+            this.label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.Location = new System.Drawing.Point(340, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(296, 29);
+            this.label.TabIndex = 1;
+            this.label.Text = "Result";
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(322, 26);
+            this.textBox.TabIndex = 2;
+            this.textBox.Text = "Input your expression";
+            this.textBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox_MouseClick);
+            this.textBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            this.textBox.Leave += new System.EventHandler(this.textBox_Leave);
+            // 
+            // labelErrors
+            // 
+            this.labelErrors.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelErrors.Location = new System.Drawing.Point(12, 93);
+            this.labelErrors.Name = "labelErrors";
+            this.labelErrors.Size = new System.Drawing.Size(624, 89);
+            this.labelErrors.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Errors";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(648, 191);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelErrors);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label labelErrors;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ tayak_lab1/tayak_lab1/Form1.cs
@@ -0,0 +1,463 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace tayak_lab1
+{
+    public partial class Form1 : Form
+    {
+        enum symbolType { UN_OPERAND, STRONG_OPERAND, WEAK_OPERAND, LETTER_OPERAND, BRACKET, DIGIT, COMMA, LETTER, DOT, INVALID }
+
+        //*******************************красивый интерфейс*************************************//
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (textBox.Text == "Input your expression") textBox.Text = "";
+        }
+
+        private void textBox_Leave(object sender, EventArgs e)
+        {
+            if (textBox.Text == "") textBox.Text = "Input your expression";
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            labelErrors.Text = "";
+            label.Text = "";
+            Analizing();
+        }
+
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Return) button_Click(sender, e);
+        }
+
+        //**************************************Логика*******************************//
+        private void Analizing() 
+        {
+            if (ThereAreErrors(textBox.Text))
+            {
+                return;
+            }
+            Stack opz = new Stack();
+            if (!ComputeOPZ(opz)) return; 
+            labelErrors.Text += "\n";
+            Compute(opz);
+        }
+
+        //***************************Обратная польская запись************************//
+        private bool ComputeOPZ(Stack opz) 
+        {
+            Stack stack = new Stack();
+            Stack output = new Stack();
+            String formula = textBox.Text;
+            for (int i = 0; i < formula.Length; i++)
+            {
+                //обработка скобок
+                if (formula[i] == '(')
+                {
+                    stack.Push(formula[i]);
+                }
+                if (formula[i] == ')')
+                {
+                    while (String.Compare(Convert.ToString(stack.Peek()), "(") != 0)
+                    {
+                        output.Push(stack.Pop());
+                    }
+                    stack.Pop();
+                }
+                //обработка букв
+                if (Char.IsLetter(formula[i]))
+                {
+                    String oper = "";
+                    oper += formula[i];
+                    while (Char.IsLetter(formula[i + 1]))
+                    {
+                        oper += formula[++i];
+                    }
+                    if (TermType(oper) != symbolType.LETTER_OPERAND)
+                    {
+                        labelErrors.Text = "Встретился неизвестный оператор "+oper+"\n";
+                        return false;
+                    }
+                    int brackets = 1;
+                    int comma=0;
+                    int k = i + 2;
+                    while (brackets != 0) 
+                    {
+                        if (formula[k] == '(') brackets++;
+                        if (formula[k] == ')') brackets--;
+                        if (formula[k] == ',')
+                            if (brackets == 1 && comma == 0) comma++;
+                            else 
+                            {
+                                labelErrors.Text = "Неверный синтаксис использования оператора "+oper+"\n";
+                                return false;
+                            }
+                        k++;
+                    }
+                    if (comma == 0 || brackets > 0) 
+                    {
+                        labelErrors.Text = "Неверный синтаксис использования оператора " + oper + "\n";
+                        return false;
+                    }
+                    stack.Push(oper);
+                }
+                //обработка цифр
+                if (Char.IsDigit(formula[i]))
+                {
+                    String num = "";
+                    num += formula[i];
+                    if ((i + 1) < formula.Length)
+                        while (Char.IsDigit(formula[i + 1]) || formula[i + 1] == '.')
+                        {
+                            num += formula[++i];
+                            if ((i + 1) == formula.Length) break;
+                        }
+                    output.Push(num);
+                }
+                //обработка операций
+                if (IsOperand(Convert.ToString(formula[i])))
+                {
+                    switch (TermType(formula[i])) 
+                    {
+                        case symbolType.UN_OPERAND:
+                            if (stack.Count != 0)
+                                while (TermType(Convert.ToString(stack.Peek())) == symbolType.LETTER_OPERAND)
+                                {
+                                    output.Push(stack.Pop());
+                                    if (stack.Count == 0) break;
+                                }
+                                stack.Push(formula[i]);
+                                break;
+                        case symbolType.WEAK_OPERAND:
+                            {
+                                if (stack.Count != 0)
+                                    while (TermType(Convert.ToString(stack.Peek())) == symbolType.WEAK_OPERAND ||
+                                   TermType(Convert.ToString(stack.Peek())) == symbolType.LETTER_OPERAND ||
+                                   TermType(Convert.ToString(stack.Peek())) == symbolType.UN_OPERAND)
+                                    {
+                                        output.Push(stack.Pop());
+                                        if (stack.Count == 0) break;
+                                    }
+                                stack.Push(formula[i]);
+                                break;
+                            }
+                        case symbolType.STRONG_OPERAND:
+                            {
+                                if (i - 1 == -1) 
+                                {
+                                    String num = "";
+                                    num += formula[i];
+                                    if ((i + 1) < formula.Length)
+                                        while (Char.IsDigit(formula[i + 1]) || formula[i + 1] == '.')
+                                        {
+                                            num += formula[++i];
+                                            if ((i + 1) == formula.Length) break;
+                                        }
+                                    output.Push(num);
+                                    break;
+                                }
+                                if(formula[i]=='-' && (formula[i-1]=='(' || formula[i-1]==','))
+                                {
+                                    String num = "";
+                                    num += formula[i];
+                                    if ((i + 1) < formula.Length)
+                                        while (Char.IsDigit(formula[i + 1]) || formula[i + 1] == '.')
+                                        {
+                                            num += formula[++i];
+                                            if ((i + 1) == formula.Length) break;
+                                        }
+                                    output.Push(num);
+                                    break;
+                                }
+                                if (stack.Count != 0)
+                                    while (TermType(Convert.ToString(stack.Peek())) == symbolType.STRONG_OPERAND ||
+                                   TermType(Convert.ToString(stack.Peek())) == symbolType.LETTER_OPERAND ||
+                                   TermType(Convert.ToString(stack.Peek())) == symbolType.WEAK_OPERAND ||
+                                   TermType(Convert.ToString(stack.Peek())) == symbolType.UN_OPERAND)
+                                    {
+                                        output.Push(stack.Pop());
+                                        if (stack.Count == 0) break;
+                                    }
+                                stack.Push(formula[i]);
+                                break;
+                            }
+                    }
+                }
+                //обработка запятой
+                if (formula[i] == ',')
+                {
+                    while (String.Compare(Convert.ToString(stack.Peek()), "(") != 0)
+                        output.Push(stack.Pop());
+                }
+            }
+            for (int i = stack.Count; i > 0; i--)
+                output.Push(stack.Pop());
+            for (int i = output.Count; i > 0; i--)
+                opz.Push(output.Pop());
+            return true;
+        }
+
+        private void Compute(Stack opz) 
+        {
+            while (opz.Count != 1)
+            {
+                Stack temp = new Stack();
+                while (!IsOperand(Convert.ToString(opz.Peek())))
+                    temp.Push(opz.Pop());
+                try
+                {
+                    String oper = Convert.ToString(opz.Pop());
+                    double b = Convert.ToDouble(temp.Pop());
+                    double a = Convert.ToDouble(temp.Pop());
+                    opz.Push(Operation(a, b, oper));
+                }
+                catch
+                {
+                    labelErrors.Text = "Ошибка вычисления ответа";
+                    return;
+                }
+                for (int i = temp.Count; i > 0; i--)
+                {
+                    opz.Push(temp.Pop());
+                }
+            }
+            label.Text = Convert.ToString(opz.Pop());
+        }
+
+        private String Operation(double a, double b, String oper) 
+        {
+            double c;
+            switch (oper)
+            {
+                case "+": 
+                    c=a + b;
+                    break;
+                case "-": 
+                    c=a - b;
+                    break;
+                case "*": 
+                    c=a * b;
+                    break;
+                case "/": 
+                    c=a / b;
+                    break;
+                case "log": 
+                    c=Math.Log(a, b);
+                    break;
+                case "^":
+                    c = Math.Pow(a, b);
+                    break;
+                default: c=0;
+                    break;
+            }
+            return Convert.ToString(c);
+        }
+
+        private bool ThereAreErrors(String formula) 
+        {
+            if (!IsValidSymbols(formula)) 
+            {
+                labelErrors.Text += "Встретились незнакомые символы\n";
+                return true;
+            }
+            if (!IsValidFormula(formula)) 
+            {
+                labelErrors.Text += "Неверный формат ввода\n";
+                return true;
+            }
+            CorrectMistakes(formula);
+            return false;
+        }
+
+        //****************Исправление ошибок*******************//
+        private void CorrectMistakes(String formula) 
+        {
+            if (formula[0] == '-' && formula[1] == '(') formula = formula.Insert(1, "1");
+            formula=formula.Replace(")(",")*(");
+            for (int i = 0; i < formula.Length; i++) 
+            {
+                if (formula[i] == ')' && i + 1 != formula.Length) 
+                { 
+                    if(Char.IsDigit(formula[i+1]) || Char.IsLetter(formula[i+1]))
+                        formula=formula.Insert(i+1,"*");
+                }                
+                if (formula[i] == '(' && i - 1 != -1) 
+                { 
+                    if(Char.IsDigit(formula[i-1]))
+                        formula=formula.Insert(i,"*");
+                }
+            }
+            textBox.Text = formula;
+        }
+        
+        //****************Проверка на ошибки*******************//
+        private bool IsValidFormula(String formula) 
+        {
+            int brackets = 0;
+            for (int i = 0; i < formula.Length; i++) 
+            {
+                switch (formula[i]) 
+                { 
+                    case '(':
+                        if (i + 1 != formula.Length)
+                        {
+                            if (formula[i + 1] == ')') return false;
+                        }
+                        else return false;
+                        brackets++;
+                        break;
+                    case ')':
+                        if (i - 1 != -1)
+                        {
+                            if (formula[i - 1] == '(') return false;
+                        }
+                        else return false;
+                        brackets--;
+                        break;
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '^':
+                        if (i - 1 == -1 && formula[i]!='-')
+                            return false;
+                        if (i - 1 != -1)
+                            if (!(Char.IsDigit(formula[i - 1]) || formula[i - 1] == ')' || 
+                              (formula[i] == '-' && (formula[i - 1] == '(' || formula[i - 1] == ',') )))
+                                return false;
+                        if (i + 1 == formula.Length)
+                            return false;
+                        if (!(Char.IsDigit(formula[i + 1]) || Char.IsLetter(formula[i + 1]) || 
+                          formula[i + 1] == '(' ))
+                            return false;
+                        break;
+                    case '.':
+                        if ((i - 1) == -1 || (i + 1) == formula.Length)
+                            return false;
+                        if (!(Char.IsDigit(formula[i - 1]) && Char.IsDigit(formula[i + 1])))
+                            return false;
+                        break;
+                    case ',':
+                        if ((i - 1) == -1 || (i + 1) == formula.Length)
+                            return false;
+                        if (!((Char.IsDigit(formula[i - 1]) || formula[i - 1] == ')' ) && 
+                          (Char.IsDigit(formula[i + 1]) || formula[i + 1] == '(' || formula[i + 1] == '-' || Char.IsLetter(formula[i + 1]))))
+                            return false;
+                        break;
+                    default:
+                        if (Char.IsDigit(formula[i]))
+                            if (i - 1 != -1)
+                                if (Char.IsLetter(formula[i - 1]))
+                                    return false;
+                        if (Char.IsLetter(formula[i]))
+                        {
+                            if (i + 1 == formula.Length)
+                                return false;
+                            if (!(Char.IsLetter(formula[i + 1]) || formula[i + 1] == '('))
+                                return false;
+                        }
+                        break;
+                }
+                if (brackets < 0) return false;
+            }
+            if (brackets != 0) return false;
+            return true;
+        }
+
+        private bool IsValidSymbols(String formula) 
+        {
+            for (int i = 0; i < formula.Length; i++)
+            {
+                if (TermType(formula[i]) == symbolType.INVALID) return false;
+            }
+            return true;
+        }
+
+        private symbolType TermType(Char term) 
+        {
+            switch (term) 
+            { 
+                case ')':
+                case '(':
+                    return symbolType.BRACKET;
+               case '+':
+                case '-':
+                    return symbolType.STRONG_OPERAND;
+                case '*':
+                case '/':
+                    return symbolType.WEAK_OPERAND;
+                case '^':
+                    return symbolType.UN_OPERAND; 
+                case '.':
+                    return symbolType.DOT;
+                case ',':
+                    return symbolType.COMMA;
+                default:
+                    if (Char.IsDigit(term))
+                        return symbolType.DIGIT;
+                    if (Char.IsLetter(term))
+                        return symbolType.LETTER;
+                    return symbolType.INVALID;
+            }
+        }
+        
+        private symbolType TermType(String term)
+        {
+            switch (term)
+            {
+                case ")":
+                case "(":
+                    return symbolType.BRACKET;
+                case "+":
+                case "-":
+                    return symbolType.STRONG_OPERAND;
+                case "*":
+                case "/":
+                    return symbolType.WEAK_OPERAND;
+                case "^":
+                    return symbolType.UN_OPERAND; 
+                case "log":
+                    return symbolType.LETTER_OPERAND;
+                case ".":
+                    return symbolType.DOT;
+                case ",":
+                    return symbolType.COMMA;
+                default:
+                    Regex rxNums = new Regex(@"^\d+$"); // любые цифры
+                    if (rxNums.IsMatch(term))
+                        return symbolType.DIGIT;
+                    return symbolType.INVALID;
+            }
+        }
+        
+        private bool IsOperand(String term)
+        {
+            switch (term)
+            {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "^":
+                case "log":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
